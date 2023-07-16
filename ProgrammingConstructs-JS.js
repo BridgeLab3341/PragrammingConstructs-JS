7872490280a5117e0337085790fb7056802d325b
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-16 14:27:37 +0530

    [somu]Add:Arrays constructions Demo

--- ProgrammingConstructs-JS.js
+++ ProgrammingConstructs-JS.js
@@ -36,3 +36,41 @@ while(k<=num)
     k++;
     console.log(6*k);
 }
+//5. Arrays
+const myArray=[1,2,3,4,5,6,7,8,9];
+console.log(myArray);
+console.log(myArray[0]);
+console.log(myArray.length)
+ //Array.Map
+const mapArray=myArray.map(x=>x+2);
+console.log(mapArray);
+ //Array.filter
+const filteredArray=myArray.filter(item=> item ==3 || item ==6);
+console.log(filteredArray);
+//Array.sort
+const descendingarray=myArray.sort((a,b)=>a>b? -1:1);
+const ascendingarray=myArray.sort((a,b)=>a>b? 1:-1);
+console.log(descendingarray);
+console.log(ascendingarray);
+//Array.Slice
+const slicedArray=myArray.slice(2,6);
+console.log(slicedArray);
+//Array.foreach
+const foreachArray=myArray.forEach(x=>console.log(x));
+//Array.Concat
+const alpha1=["a","b","c"];
+const alpha2=["d","e","f"];
+const concatArray=alpha1.concat(alpha2);
+console.log(concatArray);
+//Array.Reduce
+const reduceArray=myArray.reduce((total,current)=>total+current);
+console.log(reduceArray);
+//Array.find
+const findArray=myArray.find(x=>x>3);
+console.log(findArray);
+//Array.push
+const pusArray=myArray.push(10);
+console.log(pusArray);
+//Array.pop(shift and unshift)
+const popArray=myArray.pop();
+console.log(popArray);
