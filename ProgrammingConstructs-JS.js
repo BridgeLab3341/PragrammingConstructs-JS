b69c137f6bf61a32a4d8a2463ce7cab1c3005842
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-16 12:58:02 +0530

    [somu]Add:Selections Statement Constructs.

--- ProgrammingConstructs-JS.js
+++ ProgrammingConstructs-JS.js
@@ -1,5 +1,17 @@
 //Programming Constructs
+//1. Sequences statement
 var a="It is a string";
 var b=20;
 console.log(typeof(a));
 console.log(typeof(b));
+
+//2. Selection statement
+let age=25;
+if((age>18))
+{
+    console.log("Eligible for Voting");
+}
+else
+{
+    console.log("Not Eligible for Voting");
+}
